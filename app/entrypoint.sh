d25ef3dc8c1dcd0f72dcdbaecd665da1d337f559
dicenl <evdberg@i-erik.nl>
2026-02-16 14:26:45 +0000

    Fix entrypoint to show errors

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 set -eu
-
-: "${RUN_EVERY_SECONDS:=900}"  # 900 = 15 min
+: "${RUN_EVERY_SECONDS:=3600}"
 
 echo "CryptoBot loop started. Interval=${RUN_EVERY_SECONDS}s"
 while true; do
   date -u
-  python /app/bot.py || true
+  python /app/bot.py
   sleep "${RUN_EVERY_SECONDS}"
 done
